a6580183b2fb9c2578bf24953b7844608b37d5da
asyikin22 <148519441+asyikin22@users.noreply.github.com>
2024-10-25 22:49:05 +0800

    restructure project using Flask + test chatbot

---
+++ Sentiment Analysis/static/chatbot.js
@@ -0,0 +1,14 @@
+// function sendMessage() {
+//     const userInput = document.getElementById("userInput").value;
+//     const messagesDiv = document.getElementById("messages");
+
+//     //append user message
+//     messagesDiv.innerHTML += `<div>You: ${userInput}</div>`;
+
+//     //clear input
+//     document.getElementById("userInput").value = '';
+
+
+
+
+// }
